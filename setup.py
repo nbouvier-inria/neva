9d39a01c6b2c0a2dbe0c77c047e788a6c2a66b80
nbouvier-inria <nathan.bouvier@inria.fr>
2024-06-21 15:47:20 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+name="neva",
+version="0.0.0",
+author="Nathan Bouvier",
+description="A software framework for NEVA",
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: MIT",
+"Operating System :: OS Independent",
+],
+keywords=[
+""
+],
+url="TODO",
+packages=setuptools.find_packages(exclude=["graphs","lava_nc-0.9.0","example"]),
+install_requires=[
+"numpy",
+"lava-nc",
+"python-sat",
+"matplotlib",
+],
+python_requires=">=3.8.0",
+)
